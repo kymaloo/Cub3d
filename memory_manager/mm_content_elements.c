1692e75ad4b07891cba9bd745dd7cda8e2485d0f
ekrebs <ekrebs.contact@gmail.com>
2025-02-14 19:49:09 +0100

    added remark about mm_area_add_elem()

--- memory_manager/mm_content_elements.c
+++ memory_manager/mm_content_elements.c
@@ -6,12 +6,22 @@
 /*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 19:28:09 by ekrebs            #+#    #+#             */
-/*   Updated: 2025/02/14 19:42:28 by ekrebs           ###   ########.fr       */
+/*   Updated: 2025/02/14 19:48:34 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "memory_manager.h"
 
+/**
+ * @brief 
+ * 
+ * could be upgraded later if allocations are slow (as they parcour the linked list left to right,)
+ * could be upgraded with an int to the structu, signifying the nb of added elements, to directly add new of to the end.
+ * 
+ * @param mm 
+ * @param area 
+ * @param elem 
+ */
 void	mm_area_add_elem(t_mm *mm, char *area, void *elem)
 {
 	t_area_node		*area_node;
