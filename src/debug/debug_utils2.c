00ebfc80da44a615c64bf5d16c51ae3fd68fa707
ekrebs <ekrebs.contact@gmail.com>
2025-05-28 21:21:06 +0200

    WIP debug_utils norms

--- src/debug/debug_utils2.c
+++ src/debug/debug_utils2.c
@@ -6,7 +6,7 @@
 /*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 21:00:28 by ekrebs            #+#    #+#             */
-/*   Updated: 2025/05/28 21:00:47 by ekrebs           ###   ########.fr       */
+/*   Updated: 2025/05/28 21:20:36 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,45 +31,58 @@ void	print_map_tile(char map_char)
 		printf(MAGENTA"ER"RESET);
 }
 
+
+struct s_debug_map
+{
+	int max[DIM];
+	int min[DIM];
+	int	a[DIM];
+	int	b[DIM];
+	int target[DIM];
+};
+
+static void init_debug_map(struct s_debug_map *s, t_game *g, int offset)
+{
+	s->a[X] = g->player->position[X];
+	s->a[Y] = g->player->position[Y];
+	s->b[X] = g->player->position[X] + g->player->direction[X] * 0.3;
+	s->b[Y] = g->player->position[Y] + g->player->direction[Y] * 0.3;
+	s->min[X] = s->a[X] - offset;
+	s->max[X] = s->a[X] + offset;
+	s->min[Y] = s->a[Y] - offset;
+	s->max[Y] = s->a[Y] + offset;
+	if (s->min[X] < 0)
+		s->min[X] = 0;
+	if (s->max[X] > g->map->x_max -1)
+		s->max[X] = g->map->x_max -1;
+	if (s->min[Y] < 0)
+		s->min[Y] = 0;
+	if (s->max[Y] > g->map->y_max -1)
+		s->max[Y] = g->map->y_max -1;
+}
 void	print_map_player(t_game *g, int offset)
 {
-	int xmin, xmax, ymin, ymax, i, j;
-	int	xa, ya, xt, yt;
-	float xb, yb;
+	struct s_debug_map s;
+	int i, j;
 	t_player *p;
 
 	p = g->player;
-	xa = p->position[X];
-	ya = p->position[Y];
-	xb = p->position[X] + p->direction[X] * 0.3;
-	yb = p->position[Y] + p->direction[Y] * 0.3;
-	xmin = xa - offset;
-	xmax = xa + offset;
-	ymin = ya - offset;
-	ymax = ya + offset;
-	if (xmin < 0)
-		xmin = 0;
-	if (xmax > g->map->x_max -1)
-		xmax = g->map->x_max -1;
-	if (ymin < 0)
-		ymin = 0;
-	if (ymax > g->map->y_max -1)
-		ymax = g->map->y_max -1;
-	print_coord("player is:", "", xa, ya);
-	print_coord("targeting move:", "", xb, yb);
-	xt = xb;
-	yt = yb;
-	print_coord("targeting grid:", "", xt, yt);
+	set_min_max(&s, g, offset);
+	print_coord("player is:", "", s.a[X], s.a[Y]);
+	print_coord("targeting move:", "", s.b[X], s.b[Y]);
+	s.target[X] = s.b[X];
+	s.target[Y] = s.b[Y];
+	print_coord("targeting grid:", "", s.target[X], s.target[Y]);
 	printf("\ncurrent grid situation: \n");
-	j = ymin;
-	while (j <= ymax)
+	j = s.min[Y];
+	while (j <= s.max[Y])
 	{
-		i = xmin;
-		while (i <= xmax)
+		i = s.min[X];
+		while (i <= s.max[X])
 		{
-			if (i == xt && j == yt)
+			if (i == s.target[X] && j == s.target[Y])
 				printf(BGBLUE"");
-			if (i == xa && j == ya)
+			if (i == s.a[X] && j == s.a[Y])
 				printf(BBLUE"xx"RESET);
 			else
 				print_map_tile(g->map->grid[j][i]);
